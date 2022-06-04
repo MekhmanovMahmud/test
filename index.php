66f8f055d494ed9e343100652e4a4eb7dd4199c5
Mahmud <mahmudjon@gmail.com>
2022-06-04 14:42:51 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$hi='3454';
+$o='-3454';
+
+if ($o>$hi){
+    echo "it is true";
+}else{
+    echo "it is false";
+}
+
+$name='mahmud';
+$surname='mekhmanov';
+echo "<br>";
+echo $name . $surname;
+echo "<br>";
+echo $hi/$o;
+
+?>
+
+
